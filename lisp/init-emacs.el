9b409bbcf5a85dc500f967063930e61f2d181eb4
byteneco <dive2.hobby@gmail.com>
2024-03-12 18:54:36 +0900

    feat: add exec-path-from-shell

--- lisp/init-emacs.el
+++ lisp/init-emacs.el
@@ -1,3 +1,10 @@
+(use-package exec-path-from-shell
+  :ensure t
+  :if (memq window-system '(mac ns x))
+  :config
+  (setq exec-path-from-shell-variables '("PATH" "GOPATH"))
+  (exec-path-from-shell-initialize))
+
 (setq make-backup-files nil)
 (global-display-line-numbers-mode 1)
 (when (display-graphic-p) (toggle-scroll-bar -1))
