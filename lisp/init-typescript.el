f0141d9103c2438c8a3728e488acc8552a67d57d
byteneco <dive2.hobby@gmail.com>
2024-05-31 18:12:50 +0900

    feat: modify meow

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,7 +1,7 @@
 (use-package typescript-ts-mode
   :init
-  (add-to-list 'treesit-language-source-alist '(typescript "https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
-  (add-to-list 'treesit-language-source-alist '(typescript "https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
+  (add-to-list 'treesit-language-source-alist '(typescript "https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+  (add-to-list 'treesit-language-source-alist '(tsx "https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
   :hook
   (typescript-ts-mode . lsp-deferred)
   (tsx-ts-mode . lsp-deferred)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -21,6 +21,6 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabld
 	doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-nord t))
+  (load-theme 'doom-gruvbox t))
 
 (provide 'init-ui)
