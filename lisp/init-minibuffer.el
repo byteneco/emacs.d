9ee376448d83fc580055da2abea5e5ec4b9a99cf
byteneco <dive2.hobby@gmail.com>
2024-06-04 19:32:55 +0900

    feat: add key

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -37,6 +37,7 @@
          ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
          ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
          ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ("C-x p C-b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
          ;; Custom M-# bindings for fast register access
          ("M-#" . consult-register-load)
          ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
